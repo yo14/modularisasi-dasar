71062a338518665a2b817a9f2db9bb8354e9f3a2
Yolla Ifliandry <ifliandry11@gmail.com>
2020-06-19 05:58:43 +0700

    Initial commit

---
+++ start.py
@@ -0,0 +1,12 @@
+daftar_teman = []
+daftar_teman.append('Joko')
+daftar_teman.append('Prabowo')
+daftar_teman.append('Mega')
+daftar_teman.append('Gareng')
+
+
+print('Daftar Teman')
+print('-' * 12)
+for dn in daftar_teman:
+    print(dn)
+

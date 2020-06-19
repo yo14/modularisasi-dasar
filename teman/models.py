f43da4e6bff9da22ab98b7174d87af1f54847fb5
Yolla Ifliandry <ifliandry11@gmail.com>
2020-06-19 20:35:12 +0700

    Menambahkan property alamat pada class

--- teman/models.py
+++ teman/models.py
@@ -2,16 +2,22 @@ class Teman():
     def __init__(self, nama, sex):
         self.nama = nama
         self.sex = sex
+        self.alamat = 'belum terdaftar'
 
     def __str__(self):
-        return (f'Nama = {self.nama} dan Sex = {self.sex}')
+        return (f'Nama = {self.nama}, Sex = {self.sex}, dan alamat = {self.alamat}')
 
 daftar_teman = []
 
 daftar_teman.append(Teman('Prabowo','Laki-laki'))
 daftar_teman.append(Teman('Joko','Laki-laki'))
 daftar_teman.append(Teman('Mega','Laki-laki'))
-daftar_teman.append(Teman('Gareng','Tidak tau'))
+
+gareng = Teman('Gareng','Tidak tau')
+gareng.alamat = 'Nirwana Gang Kudus Nomor 212'
+daftar_teman.append(gareng)
+
+
 
 
 
